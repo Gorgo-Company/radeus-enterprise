ff03fd771eaa17e57bba2d8b19874dd33e71a114
Gorgo-Company <umbrella@gorgocompany.com>
2026-04-20 00:36:17 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./styles.css";
-import { App } from "./app/App";
+import { App } from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
